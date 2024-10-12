f92ba993934cea0b333c7221b52f6cd5002ca281
Gaurav Singh <gauravprofessional786@gmail.com>
2024-10-13 00:38:24 +0530

    Create evenodd.c

---
+++ evenodd.c
@@ -0,0 +1,16 @@
+// prime number program in c language 
+#include <stdio.h>
+int main( )
+{
+int n;
+printf("Enter the number you wish to enter:\n");
+scanf("%d",&n);
+if (n%2==0){
+printf("The %d is Even number,n);
+}
+else
+{
+printf("The %d is odd number",n);
+}
+return 0;
+}
